338051092d630d40584b955f74859c5cd621e4e4
zhranhazm06-source <zhranhazm06@gmail.com>
2026-07-28 15:28:03 +0300

    Create news_sources.py

---
+++ news_sources.py
@@ -0,0 +1,14 @@
+SOURCES = [
+    {
+        "name": "وزارة الإسكان",
+        "url": "https://www.mhuc.gov.eg"
+    },
+    {
+        "name": "هيئة المجتمعات العمرانية",
+        "url": "https://www.newcities.gov.eg"
+    },
+    {
+        "name": "صندوق الإسكان الاجتماعي",
+        "url": "https://www.shmff.gov.eg"
+    }
+]
